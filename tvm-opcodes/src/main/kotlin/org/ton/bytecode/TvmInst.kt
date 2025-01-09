ea9d227a5fbdcf0a64985cd2880210144d0eaf9b
tochilinak <tochilina.2002@yandex.ru>
2025-01-09 15:50:17 +0300

    Returned TvmArtificialInst

--- tvm-opcodes/src/main/kotlin/org/ton/bytecode/TvmInst.kt
+++ tvm-opcodes/src/main/kotlin/org/ton/bytecode/TvmInst.kt
@@ -7,5 +7,20 @@ sealed interface TvmInst {
     val mnemonic: String
     val location: TvmInstLocation
     val gasConsumption: TvmGas
-    // TODO should we define opcodes?
+}
+
+/**
+ * These instructions never occur in disassembler output.
+ * They are needed for custom modifications of [TvmContractCode].
+ * */
+@Serializable
+sealed interface TvmArtificialInst : TvmInst {
+    override val gasConsumption: TvmGas
+        get() = TvmFixedGas(value = 0)
+
+    fun checkLocationInitialized() {
+        val lambdaLoc = location as? TvmInstLambdaLocation
+        // this will fail if parent is not initialized
+        lambdaLoc?.parent
+    }
 }
